d0212bb75fb55c078ac9840842f398e417b89d55
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 16:17:18 +0100

    layout files added for each screen
    main.xml was renamed splash.xml and copied for game.xml, help.xml,
    login.xml, menu.xml, scores.xml and settings.xml

--- gen/com/silke/sceneFiendAndroidApp/R.java
+++ gen/com/silke/sceneFiendAndroidApp/R.java
@@ -15,12 +15,28 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int game=0x7f030000;
+        public static final int help=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int menu=0x7f030003;
+        public static final int scores=0x7f030004;
+        public static final int settings=0x7f030005;
+        public static final int splash=0x7f030006;
     }
     public static final class string {
-        public static final int app_desc=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int app_version_info=0x7f040003;
-        public static final int hello=0x7f040000;
+        public static final int app_desc=0x7f040001;
+        /**  Generic App String Information (predominantly for splash screen) 
+         */
+        public static final int app_name=0x7f040000;
+        public static final int app_version_info=0x7f040002;
+        public static final int game=0x7f040006;
+        /**  Title screens 
+         */
+        public static final int help=0x7f040003;
+        public static final int login=0x7f040009;
+        public static final int menu=0x7f040007;
+        public static final int scores=0x7f040008;
+        public static final int settings=0x7f040005;
+        public static final int splash=0x7f040004;
     }
 }
