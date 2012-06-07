9a27b2cf1f8e25e0b0f24f72e219cb6287771c52
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 19:49:21 +0100

    added activity loading from the menu
    menu activity changes to load activity screens from their listing id

---
+++ src/com/silke/sceneFiendAndroidApp/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.silke.sceneFiendAndroidApp;
+
+import android.os.Bundle;
+
+public class LoginActivity extends SceneFiendAndroidAppActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
