27c38b4adc48621a8c50583cb56d6638cba5db7c
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 18:26:31 +0100

    added dimens xlm file  to values folder for the splash info
    version text spacing, version text size, description size and title
    size for the splash screen

--- gen/com/silke/sceneFiendAndroidApp/R.java
+++ gen/com/silke/sceneFiendAndroidApp/R.java
@@ -10,17 +10,23 @@ package com.silke.sceneFiendAndroidApp;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int desc_size=0x7f040001;
+        public static final int title_size=0x7f040000;
+        public static final int version_size=0x7f040002;
+        public static final int version_spacing=0x7f040003;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int icon=0x7f020001;
         public static final int splash=0x7f020002;
     }
     public static final class id {
-        public static final int CustomFont=0x7f050000;
-        public static final int TextViewBottomVersion=0x7f050004;
-        public static final int TextViewTopTitleDesc=0x7f050001;
-        public static final int frameLayout1=0x7f050002;
-        public static final int imageView1=0x7f050003;
+        public static final int CustomFont=0x7f060000;
+        public static final int TextViewBottomVersion=0x7f060004;
+        public static final int TextViewTopTitleDesc=0x7f060001;
+        public static final int frameLayout1=0x7f060002;
+        public static final int imageView1=0x7f060003;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -32,19 +38,19 @@ public final class R {
         public static final int splash=0x7f030006;
     }
     public static final class string {
-        public static final int app_desc=0x7f040001;
+        public static final int app_desc=0x7f050001;
         /**  Generic App String Information (predominantly for splash screen) 
          */
-        public static final int app_name=0x7f040000;
-        public static final int app_version_info=0x7f040002;
-        public static final int game=0x7f040006;
+        public static final int app_name=0x7f050000;
+        public static final int app_version_info=0x7f050002;
+        public static final int game=0x7f050006;
         /**  Title screens 
          */
-        public static final int help=0x7f040003;
-        public static final int login=0x7f040009;
-        public static final int menu=0x7f040007;
-        public static final int scores=0x7f040008;
-        public static final int settings=0x7f040005;
-        public static final int splash=0x7f040004;
+        public static final int help=0x7f050003;
+        public static final int login=0x7f050009;
+        public static final int menu=0x7f050007;
+        public static final int scores=0x7f050008;
+        public static final int settings=0x7f050005;
+        public static final int splash=0x7f050004;
     }
 }
