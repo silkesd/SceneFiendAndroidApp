53e101e42317ac2158cef00684fa39bec02d7093
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 18:23:48 +0100

    added frame for image and text views to splash screen
    text and image added to splash screen

--- gen/com/silke/sceneFiendAndroidApp/R.java
+++ gen/com/silke/sceneFiendAndroidApp/R.java
@@ -15,6 +15,13 @@ public final class R {
         public static final int icon=0x7f020001;
         public static final int splash=0x7f020002;
     }
+    public static final class id {
+        public static final int CustomFont=0x7f050000;
+        public static final int TextViewBottomVersion=0x7f050004;
+        public static final int TextViewTopTitleDesc=0x7f050001;
+        public static final int frameLayout1=0x7f050002;
+        public static final int imageView1=0x7f050003;
+    }
     public static final class layout {
         public static final int game=0x7f030000;
         public static final int help=0x7f030001;
