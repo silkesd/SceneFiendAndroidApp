d9f1d36875e47913da23983463c59f47b08b2e31
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 18:32:19 +0100

    added colours xml file to the values folder
    using colours from a new xml colours file in the values folder. these
    colours were then called for the splash screen for version text colour,
    logo colour and title colour

--- gen/com/silke/sceneFiendAndroidApp/R.java
+++ gen/com/silke/sceneFiendAndroidApp/R.java
@@ -10,11 +10,17 @@ package com.silke.sceneFiendAndroidApp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int logo_color=0x7f040000;
+        public static final int title_color=0x7f040003;
+        public static final int version_bkgrd=0x7f040002;
+        public static final int version_color=0x7f040001;
+    }
     public static final class dimen {
-        public static final int desc_size=0x7f040001;
-        public static final int title_size=0x7f040000;
-        public static final int version_size=0x7f040002;
-        public static final int version_spacing=0x7f040003;
+        public static final int desc_size=0x7f050001;
+        public static final int title_size=0x7f050000;
+        public static final int version_size=0x7f050002;
+        public static final int version_spacing=0x7f050003;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -22,11 +28,11 @@ public final class R {
         public static final int splash=0x7f020002;
     }
     public static final class id {
-        public static final int CustomFont=0x7f060000;
-        public static final int TextViewBottomVersion=0x7f060004;
-        public static final int TextViewTopTitleDesc=0x7f060001;
-        public static final int frameLayout1=0x7f060002;
-        public static final int imageView1=0x7f060003;
+        public static final int CustomFont=0x7f070000;
+        public static final int TextViewBottomVersion=0x7f070004;
+        public static final int TextViewTopTitleDesc=0x7f070001;
+        public static final int frameLayout1=0x7f070002;
+        public static final int imageView1=0x7f070003;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -38,19 +44,19 @@ public final class R {
         public static final int splash=0x7f030006;
     }
     public static final class string {
-        public static final int app_desc=0x7f050001;
+        public static final int app_desc=0x7f060001;
         /**  Generic App String Information (predominantly for splash screen) 
          */
-        public static final int app_name=0x7f050000;
-        public static final int app_version_info=0x7f050002;
-        public static final int game=0x7f050006;
+        public static final int app_name=0x7f060000;
+        public static final int app_version_info=0x7f060002;
+        public static final int game=0x7f060006;
         /**  Title screens 
          */
-        public static final int help=0x7f050003;
-        public static final int login=0x7f050009;
-        public static final int menu=0x7f050007;
-        public static final int scores=0x7f050008;
-        public static final int settings=0x7f050005;
-        public static final int splash=0x7f050004;
+        public static final int help=0x7f060003;
+        public static final int login=0x7f060009;
+        public static final int menu=0x7f060007;
+        public static final int scores=0x7f060008;
+        public static final int settings=0x7f060005;
+        public static final int splash=0x7f060004;
     }
 }
