5f168ecb73ebcf628804a20b55d30a6d5adbaf6f
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 18:40:06 +0100

    added an animation for the splash screen
    added an animation function for the splash screen to fade in and fade
    out before loading the menu screen. An anim folder was created holding
    the xml for the animation

--- src/com/silke/sceneFiendAndroidApp/SplashActivity.java
+++ src/com/silke/sceneFiendAndroidApp/SplashActivity.java
@@ -1,7 +1,15 @@
 package com.silke.sceneFiendAndroidApp;
 
+import com.silke.sceneFiendAndroidApp.MenuActivity;
+import com.silke.sceneFiendAndroidApp.R;
+import com.silke.sceneFiendAndroidApp.SplashActivity;
+
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.TextView;
 
 public class SplashActivity extends SceneFiendAndroidAppActivity {
@@ -15,5 +23,67 @@ public class SplashActivity extends SceneFiendAndroidAppActivity {
                 "fonts/lucindablack.ttf");
         TextView tv = (TextView) findViewById(R.id.CustomFont);
         tv.setTypeface(tf);
+        
+      //calling the animation
+        startAnimating();
+    }
+    
+    //starting the animation on the splash screen 
+    private void startAnimating() 
+    {
+    	
+    	// Fade in the top title
+        TextView logo1 = (TextView) findViewById(R.id.CustomFont);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in_title);
+        logo1.startAnimation(fade1);
+    
+        // Fade in the description of the game after a built-in delay in the anim folder.
+        TextView logo2 = (TextView) findViewById(R.id.TextViewTopTitleDesc);
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in_title_desc);
+        logo2.startAnimation(fade2);
+        
+        // Transition to Main Menu - WILL CHANGE TO LOGIN LATER when description finishes animating
+        fade2.setAnimationListener(new AnimationListener() 
+        {
+            public void onAnimationEnd(Animation animation) 
+            {
+            	// The animation has ended, transition to the Login screen
+            	startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+                SplashActivity.this.finish();
+            }
+
+            public void onAnimationRepeat(Animation animation) 
+            {
+            }
+
+            public void onAnimationStart(Animation animation) 
+            {
+            }
+        });
+       
+    }
+  
+    @Override
+    //if the user pauses the splqsh animation
+    protected void onPause() 
+    {
+        super.onPause();
+        // Stop the animations
+        TextView logo1 = (TextView) findViewById(R.id.CustomFont);
+        logo1.clearAnimation();
+        
+        TextView logo2 = (TextView) findViewById(R.id.TextViewTopTitleDesc);
+        logo2.clearAnimation();
+
+    }
+    
+    @Override
+    //if the user returns to the animation after pausing
+    protected void onResume() 
+    {
+        super.onResume();
+
+        // Start again from the beginning of the splash animation
+        startAnimating();
     }
 }
