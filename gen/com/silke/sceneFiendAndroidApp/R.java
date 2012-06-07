f84219687e710a2be4e11c03c3f18bb9a39accd7
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-07 21:33:21 +0100

    scores layout file and background added
    scores layout in three tabs added and a background file

--- gen/com/silke/sceneFiendAndroidApp/R.java
+++ gen/com/silke/sceneFiendAndroidApp/R.java
@@ -39,21 +39,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int bkgrnd=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int splash=0x7f020003;
-        public static final int textured=0x7f020004;
+        public static final int bkgrnd_clear=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int splash=0x7f020004;
+        public static final int textured=0x7f020005;
     }
     public static final class id {
         public static final int CustomFont=0x7f090000;
         public static final int ListView_Menu=0x7f090004;
         public static final int RelativeLayout01=0x7f090001;
         public static final int RelativeLayout02=0x7f090003;
-        public static final int TextViewBottomVersion=0x7f090008;
-        public static final int TextViewTopTitleDesc=0x7f090005;
+        public static final int ScrollViewAllScores=0x7f090006;
+        public static final int ScrollViewHighScores=0x7f09000a;
+        public static final int ScrollViewMyScores=0x7f090008;
+        public static final int TabHost1=0x7f090005;
+        public static final int TableLayout_AllScores=0x7f090007;
+        public static final int TableLayout_HighScores=0x7f09000b;
+        public static final int TableLayout_MyScores=0x7f090009;
+        public static final int TextViewBottomVersion=0x7f09000f;
+        public static final int TextViewTopTitleDesc=0x7f09000c;
         public static final int TextView_HelpText=0x7f090002;
-        public static final int frameLayout1=0x7f090006;
-        public static final int imageView1=0x7f090007;
+        public static final int frameLayout1=0x7f09000d;
+        public static final int imageView1=0x7f09000e;
     }
     public static final class layout {
         public static final int game=0x7f030000;
--- src/com/silke/sceneFiendAndroidApp/ScoresActivity.java
+++ src/com/silke/sceneFiendAndroidApp/ScoresActivity.java
@@ -1,12 +1,22 @@
 package com.silke.sceneFiendAndroidApp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ScoresActivity extends SceneFiendAndroidAppActivity {
+public class ScoresActivity extends SceneFiendAndroidAppActivity 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scores);
+        
+        
+        Typeface tf = Typeface.createFromAsset(getAssets(),
+                "fonts/lucindablack.ttf");
+        TextView tv = (TextView) findViewById(R.id.CustomFont);
+        tv.setTypeface(tf);
     }
 }
