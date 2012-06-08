0cc238e74a49b698d139d272bdf04fee7a1a7f83
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-08 18:34:27 +0100

    added game preferences player id and xml tag question strings
    game preferences player id added and xml tag question strings for the
    game play in the base activity class

--- src/com/silke/sceneFiendAndroidApp/SceneFiendAndroidAppActivity.java
+++ src/com/silke/sceneFiendAndroidApp/SceneFiendAndroidAppActivity.java
@@ -14,5 +14,15 @@ public class SceneFiendAndroidAppActivity extends Activity
 	public static final String GAME_PREFERENCES_CURRENT_QUESTION = "CurQuestion"; // Integer
 	public static final String GAME_PREFERENCES_AVATAR = "Avatar"; // String URL to image
 	
+	public static final String GAME_PREFERENCES_PLAYER_ID = "ServerId"; // Integer
+	
+	// XML tag names for questions in the game
+    public static final String XML_TAG_QUESTION_BLOCK = "questions";
+    public static final String XML_TAG_QUESTION = "question";
+    public static final String XML_TAG_QUESTION_ATTRIBUTE_NUMBER = "number";
+    public static final String XML_TAG_QUESTION_ATTRIBUTE_TEXT = "text";
+    public static final String XML_TAG_QUESTION_ATTRIBUTE_IMAGEURL = "imageUrl";
+    public static final int QUESTION_BATCH_SIZE = 5; // 15;
+	
 	protected static final String DEBUG_TAG = "Scene Fiend Log";
 }
