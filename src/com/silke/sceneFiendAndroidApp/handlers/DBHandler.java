0232f37cb8c505739dfbd03e038269747383aad0
Silke Stienen-Durand <silke_sd@hotmail.com>
2012-06-10 17:29:11 +0100

    implemented database handler for sqllite db
    database sqllite logic to store player name, id, email on login and
    register from scenefiend, not twitter

--- src/com/silke/sceneFiendAndroidApp/handlers/DBHandler.java
+++ src/com/silke/sceneFiendAndroidApp/handlers/DBHandler.java
@@ -1,8 +1,118 @@
 package com.silke.sceneFiendAndroidApp.handlers;
 
+import java.util.HashMap;
 
-public class DBHandler 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper
 {
+	// All Static variables
+	// Database Version
+	private static final int DATABASE_VERSION = 1;
 
+	// Database Name
+	private static final String DATABASE_NAME = "SceneFiendDB";
+
+	// Login table name
+	private static final String TABLE_PLAYER = "scene__player";
+
+	// Login Table Columns names
+	private static final String KEY_ID = "player_id";
+	private static final String KEY_NAME = "player_name";
+	private static final String KEY_EMAIL = "player_email";
+
+
+	public DBHandler(Context context) 
+	{
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	// Creating Tables
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String CREATE_PLAYER_TABLE = "CREATE TABLE " + TABLE_PLAYER + "("
+				+ KEY_ID + " INTEGER PRIMARY KEY," 
+				+ KEY_NAME + " VARCHAR,"
+				+ KEY_EMAIL + " VARCHAR,";
+		db.execSQL(CREATE_PLAYER_TABLE);
+	}
+
+	// Upgrading database
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Drop older table if exists
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_PLAYER);
+
+		// Create table again
+		onCreate(db);
+	}
+
+	/**
+	 * Storing user details in database
+	 * */
+	public void addUser(String player_id, String player_name, String player_email) {
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(KEY_NAME, player_name); // Name
+		values.put(KEY_EMAIL, player_email); // Email
+
+		// Inserting Row
+		db.insert(TABLE_PLAYER, null, values);
+		db.close(); // Closing database connection
+	}
+
+	/**
+	 * Getting user data from database
+	 * */
+	public HashMap<String, String> getUserDetails(){
+		HashMap<String,String> user = new HashMap<String,String>();
+		String selectQuery = "SELECT  * FROM " + TABLE_PLAYER;
+		 
+	    SQLiteDatabase db = this.getReadableDatabase();
+	    Cursor cursor = db.rawQuery(selectQuery, null);
+	    // Move to first row
+	    cursor.moveToFirst();
+	    if(cursor.getCount() > 0){
+	    	user.put("player_id", cursor.getString(1));
+	    	user.put("player_name", cursor.getString(2));
+	    	user.put("player_email", cursor.getString(3));
+	    }
+	    cursor.close();
+	    db.close();
+		// return user
+		return user;
+	}
+
+	/**
+	 * Getting user login status
+	 * return true if rows are there in table
+	 * */
+	public int getRowCount() {
+		String countQuery = "SELECT  * FROM " + TABLE_PLAYER;
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.rawQuery(countQuery, null);
+		int rowCount = cursor.getCount();
+		db.close();
+		cursor.close();
+		
+		// return row count
+		return rowCount;
+	}
+
+	/**
+	 * Re crate database
+	 * Delete all tables and create them again
+	 * */
+	public void resetTables(){
+		SQLiteDatabase db = this.getWritableDatabase();
+		// Delete All Rows
+		db.delete(TABLE_PLAYER, null, null);
+		db.close();
+	}
 
 }
